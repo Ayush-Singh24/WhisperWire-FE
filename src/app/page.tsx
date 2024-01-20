981562c6f603faa1862db8482c94fcc0e84a1f1f
Ayush Singh <asayushsingh80@gmail.com>
2024-01-20 21:12:31 +0530

    logo and button animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,79 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Variants, motion } from "framer-motion";
+
+const gojoAnimation: Variants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
+
+const container: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: 1.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const item: Variants = {
+  inital: {
+    opacity: 0,
+    y: -5,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 export default function Home() {
   return (
     <section className="h-full bg-gradient-home text-primary-text flex justify-center items-center max-w-[1550px]">
       <div className="h-full relative flex justify-center items-center">
-        <Image
-          src="/images/gojo.png"
-          height={400}
-          width={500}
-          alt="Gojo"
-          className=""
-        />
-        <div className="absolute top-O h-full flex flex-col items-center justify-evenly">
+        <motion.div
+          variants={gojoAnimation}
+          animate="animate"
+          initial="initial"
+        >
+          <Image
+            src="/images/gojo.png"
+            height={400}
+            width={500}
+            alt="Gojo"
+            className=""
+            priority={true}
+          />
+        </motion.div>
+        <motion.div
+          variants={container}
+          initial="initial"
+          animate="animate"
+          className="absolute top-O h-full flex flex-col items-center justify-evenly"
+        >
           <div className="invisible"></div>
-          <span className="tracking-[0.25rem] text-center text-5xl font-extrabold bg-gradient-text bg-clip-text text-transparent sm:text-6xl sm:tracking-[0.5rem] md:text-8xl  md:tracking-[1rem]">
+          <motion.span
+            variants={item}
+            className="tracking-[0.25rem] text-center text-5xl font-extrabold bg-gradient-text bg-clip-text text-transparent sm:text-6xl sm:tracking-[0.5rem] md:text-8xl  md:tracking-[1rem]"
+          >
             WhisperWire
-          </span>
-          <div className="flex gap-5">
+          </motion.span>
+          <motion.div variants={item} className="flex gap-5 sm:gap-7 md:gap-10">
             <Button
               className="text-xl tracking-widest font-bold px-5"
               size={"lg"}
@@ -30,8 +86,8 @@ export default function Home() {
             >
               SignUp
             </Button>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   );
