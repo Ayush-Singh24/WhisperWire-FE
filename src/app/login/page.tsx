f244fcb1da242128d9cbdd92557f972533b4b729
Ayush Singh <asayushsingh80@gmail.com>
2024-01-23 22:39:15 +0530

    initial login layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 export default function Login() {
   return (
-    <section className="h-full max-w-[1550px] justify-center items-center">
+    <section className="h-full max-w-[1550px] flex justify-center items-center">
       <div className="text-primary-text ">Hello world!</div>
     </section>
   );
