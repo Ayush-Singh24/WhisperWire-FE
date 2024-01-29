c43771758bff6984b0331116d2094cbd311e0b01
Ayush Singh <asayushsingh80@gmail.com>
2024-01-30 00:16:52 +0530

    implemented ProfileForm componentn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,12 @@
+import ProfileForm from "@/components/profileForm";
+
 export default function Login() {
   return (
     <section className="h-full max-w-[1550px] flex justify-center items-center">
-      <div className="text-primary-text ">Hello world!</div>
+      <div className="text-primary-text ">
+        <ProfileForm />
+      </div>
+      <div>Go back</div>
     </section>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,7 @@ export default function SignUp() {
   return (
     <section className="h-full max-w-[1550px] flex justify-center items-center">
       <div className="text-primary-text ">SignUp</div>
+      <div>Go back</div>
     </section>
   );
 }
