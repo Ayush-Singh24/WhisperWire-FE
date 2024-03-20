176a2b062249d325687c241d9a269ea083090af2
Ayush Singh <asayushsingh80@gmail.com>
2024-03-21 00:42:27 +0530

    added bcrypt and changed field username -> name

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -6,8 +6,8 @@ export const LoginSchema = z.object({
 
 export const SignUpSchema = z
   .object({
-    username: z.string({ required_error: "Username is required" }).min(2, {
-      message: "Username must be at least 2 characters long",
+    name: z.string({ required_error: "Username is required" }).min(2, {
+      message: "Name must be at least 2 characters long",
     }),
     email: z
       .string({ required_error: "Email is required" })
