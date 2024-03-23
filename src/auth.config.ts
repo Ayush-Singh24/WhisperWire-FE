e94740e8b0c92dd107607c58e1d94e51d5632821
Ayush Singh <asayushsingh80@gmail.com>
2024-03-24 02:32:20 +0530

    fixed imports and change bcrypt -> bcryptjs

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,10 +1,10 @@
 import Credentials from "next-auth/providers/credentials";
 
 import type { NextAuthConfig } from "next-auth";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 import { LoginSchema } from "@/schemas";
-import { getUserByEmail } from "./data/user";
+import { getUserByEmail } from "@/data/user";
 
 export default {
   providers: [
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import {
   apiAuthPrefix,
   authRoutes,
   publicRoutes,
-} from "./routes";
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
