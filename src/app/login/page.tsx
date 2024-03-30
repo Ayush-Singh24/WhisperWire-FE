178f8747bee742c297a789956a07fb2ae21c8fb8
Ayush Singh <asayushsingh80@gmail.com>
2024-03-30 16:24:56 +0530

    2FA code input field

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,13 +16,14 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { LoginSchema } from "@/schemas";
 import { login } from "@/actions/login";
-import { useEffect, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import { useSearchParams } from "next/navigation";
 import Social from "@/components/social";
 
 export default function Login() {
   const [isPending, setTransition] = useTransition();
+  const [showTwoFactor, setShowTwoFactor] = useState<boolean>(false);
   const { toast } = useToast();
   const searchParams = useSearchParams();
   const urlError =
@@ -49,13 +50,19 @@ export default function Login() {
 
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
     setTransition(() => {
-      login(values).then((data) => {
-        if (data) {
-          toast({
-            description: data.message,
-          });
-        }
-      });
+      login(values)
+        .then((data) => {
+          if (data) {
+            toast({
+              description: data.message,
+            });
+
+            if (data.twoFactor) {
+              setShowTwoFactor(true);
+            }
+          }
+        })
+        .catch(() => toast({ description: "Something went wrong!" }));
     });
   };
 
@@ -64,50 +71,73 @@ export default function Login() {
       <div className="text-primary-text w-full p-10 flex flex-col gap-5 bg-primary-color-dark border-2 border-primary-color-light max-w-[500px] rounded-3xl">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-3xl">Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Enter email"
-                      disabled={isPending}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-3xl">Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Enter password"
-                      {...field}
-                      type="password"
-                      disabled={isPending}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    Set a password for your account.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Link href="/reset" className="underline underline-offset-4">
-              forgot password?
-            </Link>
+            {showTwoFactor && (
+              <FormField
+                control={form.control}
+                name="code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-3xl">Two Factor Code</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="123456"
+                        disabled={isPending}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            {!showTwoFactor && (
+              <>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-3xl">Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter email"
+                          disabled={isPending}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-3xl">Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter password"
+                          {...field}
+                          type="password"
+                          disabled={isPending}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        Set a password for your account.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Link href="/reset" className="underline underline-offset-4">
+                  forgot password?
+                </Link>
+              </>
+            )}
             <div className="flex justify-between items-center">
               <Button type="submit" disabled={isPending}>
                 Login
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -6,6 +6,7 @@ export const ResetSchema = z.object({
 export const LoginSchema = z.object({
   email: z.string({ required_error: "Email is required." }).email(),
   password: z.string().min(1, { message: "Password is required" }),
+  code: z.optional(z.string()),
 });
 
 export const SignUpSchema = z
