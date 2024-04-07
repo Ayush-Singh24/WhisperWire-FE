b58167821919fc1e97ef60c0e8e6b3469746a844
Ayush Singh <asayushsingh80@gmail.com>
2024-04-07 23:36:15 +0530

    basic home page layout

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -3,15 +3,18 @@ import { auth, signOut } from "@/auth";
 export default function Home() {
   const session = auth();
   return (
-    <div className="text-white">
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Log out</button>
-      </form>
-    </div>
+    <section className="w-screen h-screen flex">
+      <div className="w-1/4 bg-green-400 p-10">
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit">Log out</button>
+        </form>
+      </div>
+      <div className="w-3/4 bg-orange-500 p-10">sup</div>
+    </section>
   );
 }
