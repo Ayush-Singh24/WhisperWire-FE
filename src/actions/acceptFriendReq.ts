73b826930064bb75f55947b6fcd67373f0889323
Ayush Singh <asayushsingh80@gmail.com>
2024-05-12 00:16:15 +0530

    feat: accepting friend request

--- src/actions/acceptFriendReq.ts
+++ src/actions/acceptFriendReq.ts
@@ -4,30 +4,49 @@ import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 
 export const acceptFriendReq = async (receiverId: string, senderId: string) => {
-  const user = await db.user.findUnique({
+  const receiver = await db.user.findUnique({
     where: { id: receiverId },
     select: { id: true, friends: true },
   });
-  if (!user) {
+  if (!receiver) {
     return { message: "User not found!" };
   }
-  const sender = await db.user.findUnique({ where: { id: senderId } });
+  const sender = await db.user.findUnique({
+    where: { id: senderId },
+    include: {
+      friends: true,
+    },
+  });
+
   if (!sender) {
     return { message: "Sender not found!" };
   }
 
-  // await db.user.update({
-  //   where: { id: user.id },
-  //   data: {
-  //     friends: {
-  //       set: [...user.friends, sender],
-  //     },
-  //   },
-  // });
+  await db.user.update({
+    where: {
+      id: receiver.id,
+    },
+    data: {
+      friends: {
+        connect: [{ id: sender.id }],
+      },
+    },
+  });
+
+  await db.user.update({
+    where: {
+      id: sender.id,
+    },
+    data: {
+      friends: {
+        connect: [{ id: receiver.id }],
+      },
+    },
+  });
 
   await db.friendRequest.delete({
     where: {
-      senderId_receiverId: { senderId: sender.id, receiverId: user.id },
+      senderId_receiverId: { senderId: sender.id, receiverId: receiver.id },
     },
   });
   revalidatePath("/requests");
